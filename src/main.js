beb8045270ffe610767a2aba4b324e22302340e4
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-01-30 22:28:04 +0530

    feat: :sparkles:

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Lara from "@/presets/lara";
+import router from "./router.js";
 
 const app = createApp(App);
 
+app.use(router);
 app.use(PrimeVue, { unstyled: true, pt: Lara });
 app.mount("#app");
