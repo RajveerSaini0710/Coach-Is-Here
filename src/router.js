5641200f95b26d97f360234d072d912c30ad02fd
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-01-30 22:25:49 +0530

    feat: :sparkles: router is implemented

---
+++ src/router.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/', redirect: '/coaches' },
+		{
+			path: '/coaches',
+			name: 'CoachesList',
+			component: () => import('./pages/coaches/CoachesList.vue'),
+		},
+		{
+			path: '/coaches/:id',
+			name: 'CoachDetail',
+			component: () => import('./pages/coaches/CoachDetail.vue'),
+			children: [
+				{
+					path: '/contact',
+					name: 'ContactCoach',
+					component: () => import('./pages/requests/ContactCoach.vue'),
+				},
+			],
+		},
+		{
+			path: '/register',
+			name: 'CoachesRegistration',
+			component: () => import('./pages/coaches/CoachRegistration.vue'),
+		},
+		{
+			path: '/requests',
+			name: 'RequestReceived',
+			component: () => import('./pages/requests/RequestReceived.vue'),
+		},
+		{
+			path: '/:notFound(.*)',
+			name: 'NotFoundPage',
+			component: () => import('./pages/NotFound.vue'),
+		},
+	],
+})
+
+export default router
