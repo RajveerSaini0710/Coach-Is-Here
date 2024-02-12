c3997fce65b218cec6aef42e531dceda690d75d8
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-02-12 18:51:11 +0530

    feat: auth completed next step is auto login

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from './stores/store.js'
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -26,16 +27,19 @@ const router = createRouter({
 			path: '/register',
 			name: 'CoachesRegistration',
 			component: () => import('./pages/coaches/CoachRegistration.vue'),
+			meta: { requireAuth: true },
 		},
 		{
 			path: '/requests',
 			name: 'RequestReceived',
 			component: () => import('./pages/requests/RequestReceived.vue'),
+			meta: { requireAuth: true },
 		},
 		{
 			path: '/auth',
 			name: 'UserAuth',
 			component: () => import('./pages/auth/UserAuth.vue'),
+			meta: { requireUnauth: true },
 		},
 		{
 			path: '/:notFound(.*)',
@@ -43,6 +47,15 @@ const router = createRouter({
 			component: () => import('./pages/NotFound.vue'),
 		},
 	],
+})
+router.beforeEach((to, _from, next) => {
+	if (to.meta.requireAuth && !store.getters.isAuthenticated) {
+		next('/auth')
+	} else if (to.meta.requireUnauth && store.getters.isAuthenticated) {
+		next('/coaches')
+	} else {
+		next()
+	}
 })
 
 export default router
