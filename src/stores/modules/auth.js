fbf6bf489163899c3bf507dbce2c6fbecfbf1d40
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-02-13 12:51:38 +0530

    feat:

--- src/stores/modules/auth.js
+++ src/stores/modules/auth.js
@@ -54,6 +54,7 @@ const authModule = {
 			await axios
 				.post(url, Payload)
 				.then((res) => {
+					// const expiresIn = 5000
 					const expiresIn = +res.data.expiresIn * 1000
 					const expirationDate = new Date().getTime() + expiresIn
 
