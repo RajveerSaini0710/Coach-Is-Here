bc51db478fc5d8dcc20d705771ff4c689e1856a1
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-02-16 00:45:25 +0530

    feat:

--- src/components/coaches/CoachCount.vue
+++ src/components/coaches/CoachCount.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="flex items-center">
-		<p class="text-purple-700 font-medium">Total Coaches : {{ coachCount }}</p>
+		<p class="text-purple-700 text-xs md:text-lg font-medium">Total Coaches : {{ coachCount }}</p>
 		<!-- <p class="text-purple-700 font-medium">Specialities : {{ coachSpecialties }}</p> -->
 	</section>
 </template>
