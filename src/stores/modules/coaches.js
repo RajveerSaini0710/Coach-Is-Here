169cc9167105262611a84491c5b31cddd18654e9
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-02-27 17:16:42 +0530

    feat:

--- src/stores/modules/coaches.js
+++ src/stores/modules/coaches.js
@@ -30,7 +30,7 @@ const coachesModule = {
 				hourlyRate: payload.hourly_rate,
 			}
 			await axios
-				.put(`https://saini-lifters-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` + token, coachData)
+				.put(`https://coach-is-here-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` + token, coachData)
 				.then((res) => {
 					console.log(res)
 				})
@@ -47,7 +47,7 @@ const coachesModule = {
 				return
 			}
 			await axios
-				.get(`https://saini-lifters-default-rtdb.firebaseio.com/coaches.json`)
+				.get(`https://coach-is-here-default-rtdb.firebaseio.com/coaches.json`)
 				.then((res) => {
 					let data = res.data
 					let coaches = []
